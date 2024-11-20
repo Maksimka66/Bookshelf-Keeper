0e8507fdbe94db7fc98677f14c93a6d0418dfb27
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2024-11-20 12:45:01 +0200

    styles: join font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import { montserrat } from '@/styles/fonts';
+import cls from './layout.module.scss';
 
 export const metadata = {
     title: 'Next.js',
@@ -8,7 +10,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang='en'>
-            <body>{children}</body>
+            <head>
+                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+                <title>Wait...</title>
+                <link rel='icon' type='image/svg+xml' href='/vercel.svg' />
+                <link rel='apple-touch-icon' href='/vercel.svg' type='image/svg+xml' />
+            </head>
+            <body className={`${montserrat.className} ${cls.container}`}>{children}</body>
         </html>
     );
 }
--- src/styles/fonts/index.tsx
+++ src/styles/fonts/index.tsx
@@ -1,7 +1,8 @@
-import { Urbanist } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 
-export const urbanist = Urbanist({
+export const montserrat = Montserrat({
     weight: ['300', '400', '500', '600'],
+    style: 'normal',
     subsets: ['latin'],
-    variable: '--font-urbanist'
+    variable: '--font-montserrat'
 });
